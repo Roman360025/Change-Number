5362546de0a3a5d8ff500ea9e01e18d97696ea14
Roman <professor360025@gmail.com>
2021-02-24 00:40:16 +0300

    Добавлены новые проверки: проверка числа на натуральность, проверка на пустоту JSON, проверка числа на неотрицательное целое число

--- main/views.py
+++ main/views.py
@@ -67,6 +67,8 @@ class NumberChange(APIView):
     def post(self, request):
         number_api = NumberChangeSerializer(data=request.data)
         if number_api.is_valid():
+            if type(request.data["number"]) != int or request.data["number"] < 0:
+                return Response({"error": "Ожадилось неотрицательное целое число"})
             form_base = Number.objects.all()  # Данные, доступные в БД
             if not form_base:
                 number_api.save()
@@ -74,16 +76,16 @@ class NumberChange(APIView):
             form_base = Number.objects.all()[0]
             number = form_base.number
             if number == request.data["number"]:
-                error = 'ОШИБКА! Такое число уже вводилось'
-                number = "Ошибка"
                 logger.error('ОШИБКА! Такое число уже вводилось')
-                return Response({"error": 422})
+                return Response({"error": 'ОШИБКА! Такое число уже вводилось'})
             elif (request.data["number"] - number) == -1:
                 error = 'ОШИБКА! Поступившее число на единицу меньше обработанного числа'
                 number = "Ошибка"
                 logger.error('ОШИБКА! Поступившее число на единицу меньше обработанного числа')
-                return Response({"error": 422})
+                return Response({"error": 'ОШИБКА! Поступившее число на единицу меньше обработанного числа'})
             else:
                 form_base.number = request.data["number"]
                 form_base.save()
                 return Response({"number": request.data["number"]+1})
+        else:
+            return Response({'error': "Нет числа"})
