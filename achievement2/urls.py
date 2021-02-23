284d3aa508d6a17b76ad6847b6a479e76e89a255
Roman <professor360025@gmail.com>
2021-02-24 00:37:35 +0300

    Изменён формат API

--- achievement2/urls.py
+++ achievement2/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('api-numb/', include('rest_framework.urls')),
-    path('api/v1/', include('main.urls')),
+    # path('api-numb/', include('rest_framework.urls')),
+    path('api/v1/main/', include('main.urls')),
 ]
